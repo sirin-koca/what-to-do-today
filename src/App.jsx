860e38b0c67ec1ed56ee1c7f13d937048130a4b1
Sirin Koca <sirinkoc@uio.no>
2026-03-25 18:14:09 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     /*   { id: 1, title: "Study React", completed: false }, */
   
     { id: 1, title: "Work on My VITE/React Project", completed: false },
-    { id: 2, title: "Complete Part3:Unit Testing og IN4240-ASN1 - deadline⚡"},
+    { id: 2, title: "Complete Part3:Unit Testing IN4240-ASN1-deadline⚡"},
     { id: 3, title: "Group meeting tomorrow: Kl.12 @ SIMULA"},
     { id: 4, title: "Walk with Maya `♡´"},
     { id: 5, title: "Shopping & cooking"},
